a6b16e0682f0ebcdfca7ed39e35143da91b7f07c
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-22 03:12:10 +0200

    feat: add new AnimalCard component

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,5 @@
+import "./AnimalCard.css";
+
+export default function AnimalCard() {
+    return <h2>Animal</h2>;
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import data from "./data";
+import AnimalCard from "../AnimalCard/AnimalCard";
 
 function App() {
-    return <div className="App">Hello, world</div>;
+    return (
+        <div className="wrapper">
+            <h1>Animals</h1>
+            {data.map((animal) => (
+                <AnimalCard key={animal.name} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
