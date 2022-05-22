16e8b6939e4a6aacb1e0fd2be5798af1cdf71a21
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-22 04:12:43 +0200

    style: add style to AnimalCard.css

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -9,12 +9,14 @@ export default function AnimalCard({
     size,
 }) {
     return (
-        <div>
+        <div className="animal-wrapper">
             <h2>{name}</h2>
             <h3>{scientificName}</h3>
             <h4>{size}kg</h4>
             <div>{diet.join(", ")}</div>
-            <button onClick={() => showAdditional(additional)}>More info</button>
+            <button onClick={() => showAdditional(additional)}>
+                More info
+            </button>
         </div>
     );
 }
